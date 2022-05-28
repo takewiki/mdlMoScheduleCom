e13df3229d1807a483fe99321c198798a894aaab
takewiki <hulilei@takewiki.com.cn>
2022-05-28 16:47:14 +0800

    initial setup

---
+++ mdlMoScheduleCom_ui.R
@@ -0,0 +1,26 @@
+tabItem(tabName = "mdlMoScheduleCom",
+                    fluidRow(
+                      column(width = 12,
+                             tabBox(title ="生产订单排产管理工作台",width = 12,
+                                    id='tabSet_mdlMoScheduleCom',height = '300px',
+                                     mdlMultipleMaterialUI::prdGenUI(),
+                                    mdlMultipleMaterialUI::prdSelUI(),
+                                    
+                                    mdlMultipleMaterialUI::prdCategoryUI(),
+                                    
+                                    mdlMultipleMaterialUI::prdGroupUI(),
+                                    mdlMultipleMaterialUI::propCategoryUI(),
+                                    mdlMultipleMaterialUI::propCategoryConfigUI(),
+                                    mdlMultipleMaterialUI::propValueUI()
+                                    
+                                    ,
+                                    mdlMultipleMaterialUI::propValueConfigUI()
+                                    
+                                    
+                                    
+                                    
+                             )
+                      )
+                    )
+)
+ 
